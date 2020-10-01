06b836dc46d597ad1bbb0fc2784f2018d2c4d1bd
douglas1777 <douglas.santana1777@gmail.com>
2020-10-01 09:31:23 -0300

    Criando classe aluno e classe Curso

---
+++ src/com/br/digitalhouse/desafio/Aluno.kt
@@ -0,0 +1,4 @@
+package com.br.digitalhouse.desafio
+
+class Aluno (val name:String, val sobrenome:String, val cdAluno: Int) {
+}
---
+++ src/com/br/digitalhouse/desafio/Curso.kt
@@ -0,0 +1,4 @@
+package com.br.digitalhouse.desafio
+
+class Curso (val nomeCurso: String, val cdCurso: Int) {
+}
