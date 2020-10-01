a140645a74c410f7ffa7c363664b7ae2440bd231
douglas1777 <douglas.santana1777@gmail.com>
2020-10-01 10:05:48 -0300

    Criando a classse Digital House Manager, e seus atributos

---
+++ src/com/br/digitalhouse/desafio/DigitalHouseManager.kt
@@ -0,0 +1,9 @@
+package com.br.digitalhouse.desafio
+
+class DigitalHouseManager {
+    var listaAluno = mutableListOf<Aluno>()
+    var listaProfessorAdjunto = mutableListOf<ProfessorAdjunto>()
+    var listaProfessorTitular = mutableListOf<ProfessorTitular>()
+    var listaCurso = mutableListOf<Curso>()
+    var listaMatricula = mutableListOf<Matricula>()
+}
