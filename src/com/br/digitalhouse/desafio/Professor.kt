ef9dd4fc265b2991c23e2952029dfe11b74f8856
douglas1777 <douglas.santana1777@gmail.com>
2020-10-01 09:40:37 -0300

    Modificando a classe Professor, para classe abstrata, para que as classes filhas herdem seus atributos, também criei a classe professor adjunto, e professsor titular

--- src/com/br/digitalhouse/desafio/Professor.kt
+++ src/com/br/digitalhouse/desafio/Professor.kt
@@ -1,4 +1,4 @@
 package com.br.digitalhouse.desafio
 
-class Professor (override val nome: String, override val sobrenome: String):Pessoa {
+abstract class Professor (override val nome: String, override val sobrenome: String):Pessoa {
 }
---
+++ src/com/br/digitalhouse/desafio/ProfessorTitular.kt
@@ -0,0 +1,4 @@
+package com.br.digitalhouse.desafio
+
+class ProfessorTitular(nome: String, sobrenome: String, val especialidade: String) : Professor(nome, sobrenome) {
+}
